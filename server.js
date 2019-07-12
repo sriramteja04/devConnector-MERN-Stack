8304dc88459d725fc919a07621bd00079b65381a
sriramteja04 <s531524@nwmissouri.edu>
2019-07-12 12:21:27 -0500

    included the production files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 //connected Database
@@ -7,16 +8,22 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-  
-app.get('/', (req, res) => {
-  res.send('API Started Working');
-});
 
 //Define Routes.
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set Static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
